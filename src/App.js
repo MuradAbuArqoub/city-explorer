f5b21c29a399e8b174fcf64a4ea1be96d707d2b4
MuradAbuArqoub <muradaboarkob2000@gmail.com>
2021-09-19 22:28:11 +0300

    lab06

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import "./assets/style.css";
 
 class App extends Component {
 
@@ -37,10 +38,10 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>City explorer app</h1>
+        <h1 className="box2" >City explorer app</h1>
         {/* <button onClick={this.getLocFun}>Get Location</button> */}
 
-        <form onSubmit={this.getLocFun}>
+        <form onSubmit={this.getLocFun} className="box2">
           <input type="text" name='city' />
           <input type="submit" value='get city info' />
         </form>
@@ -48,15 +49,15 @@ class App extends Component {
         {
         this.state.showLocInfo && 
         <>
-        <p>city name: {this.state.searchQuery}</p>
-        <p>latitude: {this.state.locationResult.lat}</p>
-        <p>longitude: {this.state.locationResult.lon}</p>
+        <p className="box">city name: {this.state.searchQuery}</p>
+        <p className="box">latitude: {this.state.locationResult.lat}</p>
+        <p className="box">longitude: {this.state.locationResult.lon}</p>
 
         <img src={`https://maps.locationiq.com/v3/staticmap?
          key=pk.f66665537f53879fdc8ae34a0690b3a2&center=%3Clatitude%3E,%3Clongitude%3E&
          center=${this.state.locationResult.lat},${this.state.locationResult.lon}
          &zoom=7&size=700x500`}
-         alt="city" />
+         alt="city" className="box3" />
          </>
         }
 
